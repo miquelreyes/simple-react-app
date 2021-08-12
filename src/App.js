1f5ca75b286eb6fe01b9276be8ac06e2129e7805
Miquel Reyes <mreyescastells@gmail.com>
2021-08-12 16:46:49 +0200

    Implemented holiday fetching, filtering and autocomplete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
+import { List, AutoComplete } from 'antd';
+import './App.css';
 import { HolidayAPI } from 'holidayapi';
+import { useEffect, useState } from "react";
+import _ from "lodash";
 const key = '3fcaf5be-c201-4a4d-8644-8669aac1d051'
 const holidayApi = new HolidayAPI({ key });
-holidayApi.holidays({
-  country: 'ES',
-  year: '2020',
-});
-
-import logo from './logo.svg';
-import './App.css';
+const country = "ES";
+const year = "2020";
+const url = holidayApi.baseUrl + "holidays?key=" + key + "&country=" + country + "&year=" + year;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [holidays, setHolidays] = useState([]);
+    const [filteredHolidays, setFilteredHolidays] = useState([]);
+    const [text, setText] = useState("");
+
+    function filter(input) {
+        console.log(input);
+        setText(input);
+        setFilteredHolidays(holidays.filter(holiday => holiday.name.toLowerCase().includes(input.toLowerCase())));
+        console.log(filteredHolidays);
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            let holidays = await fetch(url);
+            holidays = await holidays.json();
+
+            const result = holidays.holidays.map((holiday) => {return holiday;});
+            console.log(holidays);
+            console.log(result);
+            setHolidays(result);
+            setFilteredHolidays(result);
+        }
+        fetchData();
+    },
+    []);
+    return (
+        <div className="App">
+            <AutoComplete
+                className = "SearchBox"
+                style={{width: 200,}}
+                options={holidays.map(holiday => {return {value: holiday.name}})}
+                placeholder="Type to filter"
+                onChange = {_.debounce(filter, 500)}
+                filterOption={(inputValue, option) =>
+                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                }
+            />
+            <List
+                className = "HolidayList"
+                size="large"
+                bordered
+                dataSource={filteredHolidays}
+                renderItem={holiday =>
+                <List.Item key={holiday.uuid}>
+                    <List.Item.Meta
+                        title = {holiday.name}
+                        description = {holiday.date}/>
+                        {holiday.weekday.date.name}
+                </List.Item>}
+            />
+        </div>
+    );
 }
 
 export default App;
