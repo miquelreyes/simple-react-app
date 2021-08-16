7f237f0f1945abfeea8fd54ba37589200e11c091
Miquel Reyes <mreyescastells@gmail.com>
2021-08-16 10:34:11 +0200

    Now using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { List, AutoComplete } from 'antd';
 import './App.css';
 import { HolidayAPI } from 'holidayapi';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import _ from "lodash";
 const key = '3fcaf5be-c201-4a4d-8644-8669aac1d051'
 const holidayApi = new HolidayAPI({ key });
@@ -11,27 +11,28 @@ const url = holidayApi.baseUrl + "holidays?key=" + key + "&country=" + country +
 
 function App() {
     const [holidays, setHolidays] = useState([]);
-    const [text, setText] = useState("");
+    const [filtered, dispatch] = useReducer(filter, []);
 
-    function filter() {
+    function filter(state, text) {
         var filteredHolidays = holidays.filter(holiday => holiday.name.toLowerCase().includes(text.toLowerCase()));
         console.log(filteredHolidays);
         return filteredHolidays;
     }
 
-    useEffect(() => {
-        async function fetchData() {
-            let holidays = await fetch(url);
-            holidays = await holidays.json();
+    async function fetchData() {
+        let holidays = await fetch(url);
+        holidays = await holidays.json();
 
-            const result = holidays.holidays.map((holiday) => {return holiday;});
-            console.log(holidays);
-            console.log(result);
-            setHolidays(result);
-        }
+        const array = holidays.holidays.map((holiday) => {return holiday;});
+        setHolidays(array);
+        dispatch("");
+    }
+
+    useEffect(() => {
         fetchData();
     },
     []);
+
     return (
         <div className="App">
             <AutoComplete
@@ -39,16 +40,16 @@ function App() {
                 style={{width: 200,}}
                 options={holidays.map(holiday => {return {value: holiday.name}})}
                 placeholder="Type to filter"
-                onChange = {_.debounce(setText, 500)}
+                onChange = {_.debounce(dispatch, 500)}
                 filterOption={(inputValue, option) =>
-                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                option.value.toLowerCase().includes(inputValue.toLowerCase())
                 }
             />
             <List
                 className = "HolidayList"
                 size="large"
                 bordered
-                dataSource={filter()}
+                dataSource={filtered}
                 renderItem={holiday =>
                 <List.Item key={holiday.uuid}>
                     <List.Item.Meta
